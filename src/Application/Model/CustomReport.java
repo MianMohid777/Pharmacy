067a1acd117fa90cdd32dd38481cd03eed627b0d
Mohid <mohid.jillani@gmail.com>
2023-12-04 22:31:25 +0500

    Pharmacy POS

--- src/Application/Model/CustomReport.java
+++ src/Application/Model/CustomReport.java
@@ -1,8 +1,6 @@
 package Application.Model;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -78,28 +76,58 @@ public class CustomReport extends Report  implements SalesReport{
 
         int rowIndex = 0;
 
+
         for (String name : categoryName) {
             if (!Objects.equals(name, "TOTAL")) {
                 Vector<Object> data = super.getReportData().get(name);
 
-                Row categoryRow =  sheet.createRow(rowIndex++);
-                categoryRow.createCell(0).setCellValue("Category Name: " + name);
+                // Create a header row for each category
+                Row headerRow = sheet.createRow(rowIndex++);
+                CellStyle headerCellStyle = workbook.createCellStyle();
+                Font headerFont = workbook.createFont();
+                headerFont.setBold(true);
+                headerCellStyle.setFont(headerFont);
+                headerCellStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex()); // Set cell background color
+                headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
+                headerRow.createCell(0).setCellValue("Category Name");
+                headerRow.getCell(0).setCellStyle(headerCellStyle);
+
+                headerRow.createCell(1).setCellValue("Product Name");
+                headerRow.getCell(1).setCellStyle(headerCellStyle);
+
+                headerRow.createCell(2).setCellValue("Qty Sold");
+                headerRow.getCell(2).setCellStyle(headerCellStyle);
+
+                // Set column width for better visibility
+                sheet.setColumnWidth(0, 4000); // Adjust the width as needed
+                sheet.setColumnWidth(1, 6000); // Adjust the width as needed
+                sheet.setColumnWidth(2, 3000); // Adjust the width as needed
+
+
+                // Create a data row for each product in the category
                 for (int i = 0, j = 1; i < data.size(); i++) {
                     Product p = (Product) data.get(i);
                     int soldStock = (int) data.get(++i);
 
-                    Row productRow =  sheet.createRow(rowIndex++);
-                    productRow.createCell(0).setCellValue(j++);
-                    productRow.createCell(1).setCellValue(p.getName());
-                    productRow.createCell(2).setCellValue("Quantity Sold = " + soldStock);
+                    Row productRow = sheet.createRow(rowIndex++);
+                    productRow.createCell(0).setCellValue(name); // Category Name
+                    productRow.createCell(1).setCellValue(p.getName()); // Product Name
+                    productRow.createCell(2).setCellValue(soldStock); // Qty Sold
 
                     categTotal += p.getPrice() * soldStock;
                 }
 
+                // Create a total row for the category
                 Row totalRow = sheet.createRow(rowIndex++);
+                CellStyle totalCellStyle = workbook.createCellStyle();
+                totalCellStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex()); // Set cell background color for total
+                totalCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
                 totalRow.createCell(0).setCellValue("Category Total Sale:");
                 totalRow.createCell(1).setCellValue(categTotal);
+                totalRow.getCell(0).setCellStyle(totalCellStyle);
+                totalRow.getCell(1).setCellStyle(totalCellStyle);
 
                 categTotal = 0.0F;
             } else {
@@ -107,14 +135,22 @@ public class CustomReport extends Report  implements SalesReport{
             }
         }
 
+
+        Row space = sheet.createRow(rowIndex++);
         Row totalSalesRow = sheet.createRow(rowIndex);
+
+        CellStyle totalCellStyle = workbook.createCellStyle();
+        totalCellStyle.setFillForegroundColor(IndexedColors.CORAL.getIndex()); // Set cell background color for total
+        totalCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
         totalSalesRow.createCell(0).setCellValue("Total Sales:");
         totalSalesRow.createCell(1).setCellValue(total);
 
+        totalSalesRow.getCell(0).setCellStyle(totalCellStyle);
+        totalSalesRow.getCell(1).setCellStyle(totalCellStyle);
+
         try (FileOutputStream fileOut = new FileOutputStream(file)) {
             workbook.write(fileOut);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
